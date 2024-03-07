547f9f69facf5af3579c35b27573b52558e4f604
Nguyen Duy Quoc <nguyenduyquoc129829042001@gmail.com>
2024-03-07 13:58:15 +0700

    first_comment

---
+++ src/main/java/com/ndquoc/spring_mvc_auction/SpringMvcAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.ndquoc.spring_mvc_auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ndquoc/spring_mvc_auction/SpringMvcAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndquoc.spring_mvc_auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
